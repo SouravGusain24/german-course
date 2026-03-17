421c8e08648e45998100d5854cd06da499a7debc
Sourav Gusain <souravgusain832@gmail.com>
2026-03-16 23:27:20 -1200

    Updated UI: Pooja Academy name and added hero image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ export default function Footer(){
   <div className="bg-blue-900 text-white p-10 text-center">
 
   <h2 className="text-2xl mb-4">
-  German Language Academy
+  Pooja Academy
   </h2>
 
   <p>
-  Learn German with expert teachers and structured courses.
+  8920506424
   </p>
 
   <p className="mt-4 text-sm">
