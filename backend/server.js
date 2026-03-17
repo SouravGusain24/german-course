6305ec893b06130f91ac3c82956259411f4c4b19
Sourav Gusain <souravgusain832@gmail.com>
2026-03-16 18:55:45 -1200

    Fix port for Render

--- backend/server.js
+++ backend/server.js
@@ -75,6 +75,8 @@ message:"Error fetching students"
 
 
 // Start Server
-app.listen(5001,()=>{
-console.log("Server running on port 5001")
-})
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
